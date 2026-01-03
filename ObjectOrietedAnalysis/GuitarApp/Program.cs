1a619982306bc3b8f465daeb8016a61b1701dc0a
Pramod2392 <pramod2392@gmail.com>
2026-01-03 20:26:49 +0530

    Fix typo: Correct "Stratocastor" to "Stratocaster"
    Corrected the guitar model name "Stratocastor" to "Stratocaster" in multiple locations within `Program.cs`.
    
    - Updated `AddGuitar` calls in the `InitializeInventory` method.
    - Updated `requiredGuitar` initialization in the main method.
    
    These changes ensure consistency and accuracy in the guitar model name.

--- ObjectOrietedAnalysis/GuitarApp/Program.cs
+++ ObjectOrietedAnalysis/GuitarApp/Program.cs
@@ -8,7 +8,7 @@
             InitializeInventory(inventory);
 
             //
-            Guitar requiredGuitar = new Guitar("", 0, "Fender", "Stratocastor", "Electric", "Alder", "Alder");
+            Guitar requiredGuitar = new Guitar("", 0, "Fender", "Stratocaster", "Electric", "Alder", "Alder");
             var searchResult = inventory.SearchGuitar(requiredGuitar);
 
             if(searchResult != null )
@@ -32,8 +32,8 @@
         {
             // Add a few guitars to inventory
             inventory.AddGuitar("11277", 3999.95, "Collings", "CJ", "Acoustic", "Indian Rosewood", "Sitka");
-            inventory.AddGuitar("V95693", 1499.95, "Fender", "Stratocastor", "Electric", "Alder", "Alder");
-            inventory.AddGuitar("V9512", 1549.95, "Fender", "Stratocastor", "Electric", "Alder", "Alder");
+            inventory.AddGuitar("V95693", 1499.95, "Fender", "Stratocaster", "Electric", "Alder", "Alder");
+            inventory.AddGuitar("V9512", 1549.95, "Fender", "Stratocaster", "Electric", "Alder", "Alder");
             inventory.AddGuitar("122784", 5495.95, "Martin", "D-18", "Acoustic", "Mahogany", "Adirondack");
             inventory.AddGuitar("76531", 6295.95, "Gibson", "Les Paul", "Electric", "Maple", "Maple");
         }
